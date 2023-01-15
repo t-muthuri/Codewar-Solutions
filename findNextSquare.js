12a195cdc94235914c8d6e5d5b7703980cf7df34
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-01-15 19:49:07 +0300

    Refactored findNextSquare

--- findNextSquare.js
+++ findNextSquare.js
@@ -1,13 +1,6 @@
 function findNextSquare(sq) {
-    if (Math.sqrt(sq) % 1){
-    return -1;
-      }
-          return (Math.sqrt(sq) +1) **2
-  }
-  
-  //   Algorithm
-  //   find the squareroot of the argument sq - Math.sqrt()
-  //   if the is a remainder return -1 - % operator to check for remainders
-  //   if the answer is a whole integer,
-  //   add 1 +1 or ++
-  //   sq the new integer and return the value
+  return Math.sqrt(sq) % 1 ? -1 : (Math.sqrt(sq) + 1) ** 2;
+}
+
+console.log(findNextSquare(344));
+console.log(findNextSquare(144));
