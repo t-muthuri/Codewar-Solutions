4246387a6f34df658526f78fe2b690b897e336a1
Tressie Muthuri <tressiemuthuri@gmail.com>
2023-01-15 22:27:16 +0300

    refactored Solution

--- solution.js
+++ solution.js
@@ -1,13 +1,16 @@
-function solution(str){
-  let newStr = '';
-  if (str.length){
-    return (str.length % 2 === 1) ? (newStr = str + '_').match(/.{2}/g) : str.match(/.{2}/g)
-  }
-   else {
-    return [];
-  }
-}
+// function solution(str){
+//   let newStr = '';
+//   if (str.length){
+//     return (str.length % 2 === 1) ? (newStr = str + '_').match(/.{2}/g) : str.match(/.{2}/g)
+//   }
+//    else {
+//     return [];
+//   }
+// }
 
+function solution(str){
+  return (str+"_").match(/.{2}/g)||[]
+}
 console.log(solution(['abcdef']));
 console.log(solution(['abcdefg']));
 console.log(solution([]));
